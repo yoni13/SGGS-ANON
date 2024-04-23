3c0308e44666636548992b914d7e418c87dc091c
yoni <yoni@vms.lyx.nicewhite.xyz>
2024-04-23 13:35:39 +0800

    cross origin

--- app.py
+++ app.py
@@ -12,9 +12,14 @@ import urllib.request
 import hashlib
 from markupsafe import escape
 from flask_limiter import Limiter
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = b'\xc0:8!E<\x96\xe8\xff\x0b\xd5\xff\x15\xf4m\xb0<\x9b\xc5]\xd5\x03X6'
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+
 from sggsanon import api
 app.register_blueprint(api.api)
 
