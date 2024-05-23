3285a3c797c8ba4f8bcc4ea40a7ddbe521e5ca2d
yoni13 <yoni980807@gmail.com>
2024-05-23 19:18:29 +0800

    basic reactions

--- static/js/reaction_test.js
+++ static/js/reaction_test.js
@@ -1,15 +1,47 @@
 const message = document.getElementsByClassName("message")[0];
 const reaction_bar = document.getElementById("reaction");
+var rect_reactbar = reaction_bar.getBoundingClientRect();
+original_bottom = rect_reactbar.bottom;
+
+
 // This handler will be executed every time the cursor
 // is moved over a different list item
 
 function reaction_handler(event){
   var rect = message.getBoundingClientRect();
-  console.log(rect.top, rect.right, rect.bottom, rect.left);
- // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
- // we use left bottom
- // https://stackoverflow.com/questions/6802956/how-to-position-a-div-at-specific-coordinates
+  reaction_bar.style.position = "absolute";
+  reaction_bar.style.display = "block";
+  reaction_bar.style.left = rect.left + "px";
+  reaction_bar.style.top = rect.bottom - 150 + "px";
+ 
 }
 
 
 message.addEventListener("mouseover", reaction_handler);
+message.addEventListener("mouseout", function(){
+  reaction_bar.style.display = "none";
+});
+
+
+
+
+var messages = document.getElementsByClassName("reaction_emotes");
+console.log('here');
+for (var i = 0; i < messages.length; i++) {
+      messages[i].addEventListener("mouseover", function() {
+          this.style.fontSize = "50px";
+      });
+
+      messages[i].addEventListener("mouseout", function() {
+          this.style.fontSize = "30px";
+      });
+      
+      messages[i].addEventListener("click", function() {
+          fetch('/api/v1/reaction', {
+              method: 'POST',
+              body: JSON.stringify({
+                  reaction: this.innerHTML
+              })
+          })
+      });
+  }
