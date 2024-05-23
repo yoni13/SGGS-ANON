6880d311ef510c1753506f54ba502a3e07711bde
yoni13 <76271912+yoni13@users.noreply.github.com>
2024-05-23 06:02:10 +0000

    havent done, but nice

--- static/js/reaction_test.js
+++ static/js/reaction_test.js
@@ -1,11 +1,15 @@
-const test = document.getElementsByClassName("message")[0];
-
+const message = document.getElementsByClassName("message")[0];
+const reaction_bar = document.getElementById("reaction");
 // This handler will be executed every time the cursor
 // is moved over a different list item
-test.addEventListener(
-  "mouseover",
-  (event) => {
-    console.log("mouse over 2");
-  },
-  false,
-);
+
+function reaction_handler(event){
+  var rect = message.getBoundingClientRect();
+  console.log(rect.top, rect.right, rect.bottom, rect.left);
+ // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+ // we use left bottom
+ // https://stackoverflow.com/questions/6802956/how-to-position-a-div-at-specific-coordinates
+}
+
+
+message.addEventListener("mouseover", reaction_handler);
