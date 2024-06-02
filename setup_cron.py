e3f3482579180562c7015462f2abf7055867dcda
yoni13 <yoni980807@gmail.com>
2024-06-03 01:22:33 +0800

    update cron

--- setup_cron.py
+++ setup_cron.py
@@ -1,5 +1,5 @@
 from crontab import CronTab
 cron = CronTab(user='root')
-job = cron.new(command='rm -rf /opt/app/tmp/*')
-job.hour.every(1)
+job = cron.new(command='rm -rf /python-docker/tmp/*')
+job.setall('2 10 * * *')
 cron.write()
